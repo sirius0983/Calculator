1c4607a9faeb9d83fa1ce9f6eda2468a59f774ad
Rustam <sirius0983@mail.ru>
2021-02-04 19:20:39 +0500

    2

--- app.js
+++ app.js
@@ -1,25 +1,25 @@
-let myInput = document.createElement('input'),
-    myButton = document.createElement('button'),
-    par = document.createElement('p')
-myInput.setAttribute('type', 'number')
-myButton.setAttribute('type', 'button')
-par.setAttribute('type', 'result')
-myButton.innerText = 'Показать'
-par.innerText = '12'
-let body = document.querySelector('body')
-body.appendChild(myInput)
-body.appendChild(myButton)
-body.appendChild(par)
-
-myButton.addEventListener('click', function () {
-    let prise = 100,
-        age = myInput.value
-    if (age < 8) {
-        prise = 0
-    } else if (age > 7 && age < 20) {
-        prise = prise * 0.8
-    } else {
-        prise = prise
-    }
-    par.innerText = `Цена билета равна ${prise}`
-})
+// let myInput = document.createElement('input'),
+//     myButton = document.createElement('button'),
+//     par = document.createElement('p')
+// myInput.setAttribute('type', 'number')
+// myButton.setAttribute('type', 'button')
+// par.setAttribute('type', 'result')
+// myButton.innerText = 'Показать'
+// par.innerText = '12'
+// let body = document.querySelector('body')
+// body.appendChild(myInput)
+// body.appendChild(myButton)
+// body.appendChild(par)
+//
+// myButton.addEventListener('click', function () {
+//     let prise = 100,
+//         age = myInput.value
+//     if (age < 8) {
+//         prise = 0
+//     } else if (age > 7 && age < 20) {
+//         prise = prise * 0.8
+//     } else {
+//         prise = prise
+//     }
+//     par.innerText = `Цена билета равна ${prise}`
+// })
